21a82cb5dc4d96069cb0a372bf7e923a961e1ce5
unknown <cspandey3000@gmail.com>
2022-11-23 15:16:12 +0530

    ways added to create

--- src/test/java/com/tests/restAssured/PostRequest.java
+++ src/test/java/com/tests/restAssured/PostRequest.java
@@ -5,11 +5,13 @@ import static org.testng.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import com.github.javafaker.Faker;
@@ -35,42 +37,62 @@ public class PostRequest {
 	@Test(enabled = false)
 
 	public void postRequestByconvertingFileToString() throws IOException {
-		
-		String reqBody = new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir")+"/src/test/resources/jsonBodyrequest/jsonrequest.json")));
 
-		Response resp = given()
-				.contentType(ContentType.JSON)
-				.header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=")
-				.body(reqBody)
-				.log()
-				.all()
-				.post("https://restful-booker.herokuapp.com/booking");
+		String reqBody = new String(Files.readAllBytes(
+				Paths.get(System.getProperty("user.dir") + "/src/test/resources/jsonBodyrequest/jsonrequest.json")));
+
+		Response resp = given().contentType(ContentType.JSON).header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=")
+				.body(reqBody).log().all().post("https://restful-booker.herokuapp.com/booking");
 
 		int actualStatus = resp.getStatusCode();
 		assertEquals(actualStatus, 200);
 		resp.prettyPrint();
 	}
-	@Test
+
+	@Test(enabled = true)
 
 	public void postRequestByCreatingBodyUsingHashMap() throws IOException {
-		
+
 		Faker faker = new Faker();
-		
-		Map<String, Object>book = new LinkedHashMap<>();
+
+		Map<String, Object> book = new LinkedHashMap<>();
 		book.put("firstname", faker.name().firstName());
 		book.put("lastname", "tet");
 		book.put("totalprice", "12");
 		book.put("depositpaid", "true");
-		Map<String, Object>bookdates = new LinkedHashMap<>();
+		Map<String, Object> bookdates = new LinkedHashMap<>();
 		bookdates.put("checkin", "2018-01-01");
 		bookdates.put("checkout", "2019-01-01");
 		book.put("bookingdates", bookdates);
 		book.put("additionalneeds", "breakfast");
+
+		Response resp = given().contentType(ContentType.JSON).header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=")
+				.body(book).log().all().post("https://restful-booker.herokuapp.com/booking");
+
+		int actualStatus = resp.getStatusCode();
+		assertEquals(actualStatus, 200);
+		resp.prettyPrint();
+	}
+
+	@Test
+	public void postRequestByCreatingBodyUsingJsonObject(){
+
+		JSONObject book = new JSONObject();
+		Faker faker = new Faker();
 		
+		book.put("firstname", faker.name().firstName());
+		book.put("lastname", "tet");
+		book.put("totalprice", "12");
+		book.put("depositpaid", "true");
+		JSONObject bookdates = new JSONObject();
+		bookdates.put("checkin", "2018-01-01");
+		bookdates.put("checkout", "2019-01-01");
+		book.put("bookingdates", bookdates);
+		book.put("additionalneeds", "breakfast");
 		Response resp = given()
 				.contentType(ContentType.JSON)
 				.header("Authorization", "Basic YWRtaW46cGFzc3dvcmQxMjM=")
-				.body(book)
+				.body(book.toMap())
 				.log()
 				.all()
 				.post("https://restful-booker.herokuapp.com/booking");
