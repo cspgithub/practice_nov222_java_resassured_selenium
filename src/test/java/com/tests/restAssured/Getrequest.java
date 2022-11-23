fca8bfa3519691f09216f207a140ccdeac246af3
unknown <cspandey3000@gmail.com>
2022-11-23 11:33:10 +0530

    suite updated

--- src/test/java/com/tests/restAssured/Getrequest.java
+++ src/test/java/com/tests/restAssured/Getrequest.java
@@ -1,5 +1,27 @@
 package com.tests.restAssured;
 
+import static io.restassured.RestAssured.given;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
 public class Getrequest {
+
+	@Test
+
+	public void getBooks() {
+		
+		
+		Response resp = given().get("https://restful-booker.herokuapp.com/booking");
+		
+		int actualStatusCode = resp.getStatusCode();
+		Assert.assertEquals(actualStatusCode, 200);
+		System.out.println(resp.headers());
+		
+		
+
+	}
 
 }
